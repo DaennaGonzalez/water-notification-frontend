1fe1a724c075a0474be0e0a7bda027e8ad715668
Christian Zamacona <christianzamacona@gmail.com>
2022-11-16 23:46:39 -0600

    se elimina proyecto

--- src/api/handleApiError.js
+++
@@ -1,8 +0,0 @@
-export const handleApiError = ({error}) => {
-    
-    if (error.response === undefined) {
-        return "Ha ocurrido un error, contacta al administrador";
-    } else {
-        return error.response.data.errors;
-    }
-};
--- src/api/waterNotifApi.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-const apiInstance = axios.create({
-    baseURL: process.env.VUE_APP_API_URL
-})
-apiInstance.defaults.headers.common["Content-Type"] = 'application/json';
-export default apiInstance
