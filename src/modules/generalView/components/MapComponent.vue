2af50d21feb31ff72fa3d58b85f7f8126566229d
Christian Zamacona <christianzamacona@gmail.com>
2022-12-08 19:11:58 -0600

    se agrega boton de comentarios

--- src/modules/generalView/components/MapComponent.vue
+++ src/modules/generalView/components/MapComponent.vue
@@ -1,11 +1,13 @@
 <script>
 /* eslint-disable no-undef */
 import {usePlacesStore} from '@/composables'
-import MapView from '../../../components/mapview/MapView.vue'
+import MapView from '@/components/mapview/MapView.vue'
+import CommentButton from '@/components/btn-comment/CommentButton.vue'
 export default {
     name: 'MapComponent',
     components:{
-        MapView 
+        MapView,
+        CommentButton 
     },
     setup() {
         usePlacesStore()
@@ -14,5 +16,7 @@ export default {
 </script>
 
 <template>
+    <h4> Zonas afectadas </h4>
+    <comment-button/>
     <map-view />
 </template>
